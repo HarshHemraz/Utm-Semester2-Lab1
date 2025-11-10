956da6c63676da3e716a4c1b2ef24e72cca23f99
harsh <harshhemraz403@gmail.com>
2025-11-10 09:07:06 +0400

    inititial commit

---
+++ week1/Student.java
@@ -0,0 +1,32 @@
+ /*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package week1;
+ 
+/**
+ *
+ * @author SPACE4
+ */
+public class Student {
+    public String studentName;
+    public int studentID;
+    
+    public void setStudentName(String name) {
+          studentName = name;
+            
+        
+    }
+    public void setStudentId(int id) {
+           studentID = id;
+            
+    }
+    
+    public String getStudentName() {
+        return studentName;
+    }
+    public int getStudentID() {
+        return studentID;
+    }  
+    
+}
